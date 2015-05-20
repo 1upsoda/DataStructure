a8a29547d2fb64006685ba1545a400cf9e037755
1upsoda <1upsoda@gmail.com>
2015-05-20 07:53:46 -0600

    recommiting all

--- src/controller/DSController.java
+++ src/controller/DSController.java
@@ -1,6 +1,28 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Stack;
+
+import model.Things;
+
 public class DSController
 {
 
+	private Queue<Things> thingQueue;
+	private Stack<Things> thingStack;
+	private ArrayList<Things> thingList;
+	
+	public DSController()
+	{
+//		thingQueue = new Queue<Things>();
+		thingStack = new Stack<Things>();
+		thingList = new ArrayList<Things>();
+	}
+	
+	private void useStack()
+	{
+		Things test = new Things();
+		thingStack.push(test);
+	}
 }
--- src/model/Things.java
+++ src/model/Things.java
@@ -3,4 +3,17 @@ package model;
 public class Things
 {
 
+	private String name;
+	private int number;
+	private boolean scuttle;
+	public Things()
+	{
+		
+	}
+	public Things(int number, boolean scuttle)
+	{
+		this.name = "";
+		this.number = number;
+		this.scuttle = scuttle;
+	}
 }
